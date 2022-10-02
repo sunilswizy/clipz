091ca23a290af39ce9915187142dba27827bda11
sunil_swizy <sunil@iamneo.ai>
2022-10-02 11:29:22 +0530

    implenting template form done

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ICredentials {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  credentials: ICredentials = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    console.log('submitted...');
   }
-
 }
